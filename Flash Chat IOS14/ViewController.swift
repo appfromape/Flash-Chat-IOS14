b95bb2f39828312601b4a5203dd1da176e976a97
appfromape <appfromape@gmail.com>
2021-03-01 17:31:58 +0800

    Initial Commit

---
+++ Flash Chat IOS14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flash Chat IOS14
+//
+//  Created by 程式猿 on 2021/3/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
